3808e25374e98c1c30ef4bf7c6a5a6eb14d6094a
Ethan Finlay <efinclarke1993@gmail.com>
2019-03-02 13:57:20 -0500

    Registration Working

--- KnightHacks2019/app/src/main/java/com/example/knighthacks2018/LoginActivity.java
+++ KnightHacks2019/app/src/main/java/com/example/knighthacks2018/LoginActivity.java
@@ -1,33 +1,122 @@
 package com.example.knighthacks2018;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-    private Button login;
+
+public class LoginActivity extends AppCompatActivity{
+
+    private Button login, signup;
     private EditText username;
-    private  EditText password;
+    private EditText password;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                if(user != null)
+                {
+                    /*Intent intent = new Intent(LoginActivity.this,.class);
+                    startActivity(intent);
+                    finish();
+                    return;*/
+                }
+            }
+        };
+
         login = (Button) findViewById(R.id.login);
+        signup = (Button) findViewById(R.id.signup);
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
 
-        login.setOnClickListener(this);
+        login.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                final String email = username.getText().toString();
+                final String pass = password.getText().toString();
+                mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>()
+                {
+
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+
+                    }
+
+                });
+            }
+
+        });
+
+        signup.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                final String email = username.getText().toString();
+                final String pass = password.getText().toString();
+                mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if(!task.isSuccessful())
+                        {
+                            Toast.makeText(LoginActivity.this, "Registration Error :(", Toast.LENGTH_SHORT).show();
+                        }
+
+                        else
+                        {
+                            String userID = mAuth.getCurrentUser().getUid();
+                            DatabaseReference currentUserDB = FirebaseDatabase.getInstance().getReference().child("Users").child(userID);
+                            currentUserDB.setValue(true);
+                        }
+
+                    }
+
+                });
+
+            }
+
+        });
 
     }
 
     @Override
-    public void onClick(View view){
+    protected void onStart()
+    {
+        super.onStart();
+        mAuth.addAuthStateListener(firebaseAuthListener);
+    }
 
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        mAuth.removeAuthStateListener(firebaseAuthListener);
     }
 
 }
