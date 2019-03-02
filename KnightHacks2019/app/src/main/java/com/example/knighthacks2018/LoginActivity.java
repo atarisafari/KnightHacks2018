f2abab10ab558b15b47d9a8e342a1960991158da
juccalle <46544460+juccalle@users.noreply.github.com>
2019-03-02 11:20:38 -0500

    hopeful build fixes

---
+++ KnightHacks2019/app/src/main/java/com/example/knighthacks2018/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.example.knighthacks2018;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button login;
+    private EditText username;
+    private  EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login = (Button) findViewById(R.id.login);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+
+        login.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view){
+
+    }
+
+}
